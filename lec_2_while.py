bd1561d23efe5c8d45f7d68cd64f66c4b2b02823
Матвийчук Полина <72652748+thesalem-cot@users.noreply.github.com>
2020-10-24 13:41:39 +0200

    Add files via upload

---
+++ lec_2_while.py
@@ -0,0 +1,13 @@
+i=5
+while i <15:
+    print (i)
+    i = i + 2 
+
+for i in "hello world":
+    if i == " O ":
+        break
+    print(i * 2, end=" ")
+    
+for i in "hello world":
+    if i == "o":
+        print(i*2,end="")
