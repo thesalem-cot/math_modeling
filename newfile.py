e5bfa5d3a3b795307020efc63397025a578c2d04
Матвийчук Полина <72652748+thesalem-cot@users.noreply.github.com>
2020-11-14 13:25:11 +0200

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,11 @@
+
+  gravity_constant = 6.67*10**(-11) 
+  light_speed = 3 * 10**8 
+  Plancks_constant = 6.6 * 10**(-34) 
+  Dirac_constant = 1.05 * 10**(-34) 
+  elementary_charge = 1.6 * 10**(-19) 
+  Boltzmann_constant = 1.4 * 10**(-23) 
+  magnetic_constant = 1.27 * 10**(-6) 
+  electric_constant = 8.85 * 10**(-12) 
+  Coulomb_constant = 8.99 * 10**9 
+  electron_mass = 9.11 * 10**(-31)
